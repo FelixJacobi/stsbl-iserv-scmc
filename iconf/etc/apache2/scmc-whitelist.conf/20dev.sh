f4142bdef4f20031363712e1781965ce433be87a
Felix Jacobi <felix.jacobi@stsbl.de>
2017-07-09 03:10:38 +0200

    Fixed typo.

--- iconf/etc/apache2/scmc-whitelist.conf/20dev.sh
+++ iconf/etc/apache2/scmc-whitelist.conf/20dev.sh
@@ -6,7 +6,7 @@ if [ "$DevEnvironment" ]
 then
   echo
   echo "  RewriteCond %{REQUEST_URI} ^/_profiler [OR]"
-  echo "  REwriteCond %{REQUEST_URI} ^/_wdt"
+  echo "  RewriteCond %{REQUEST_URI} ^/_wdt"
   echo "  RewriteRule (.*) /usr/share/iserv/web/app_dev.php [L]"
   echo
 fi
