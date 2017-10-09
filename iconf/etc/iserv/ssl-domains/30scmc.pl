2bd9c2199f6a597866868e0b52e2bb713f1882dc
Felix Jacobi <felix.jacobi@iserv.eu>
2017-10-10 01:43:11 +0200

    Fixed reproducibility of iconf template.

--- iconf/etc/iserv/ssl-domains/30scmc.pl
+++ iconf/etc/iserv/ssl-domains/30scmc.pl
@@ -5,9 +5,5 @@ use strict;
 use utf8;
 use IServ::DB;
 
-my @domains = IServ::DB::SelectCol "SELECT WebDomain FROM scmc_servers";
-
-for (@domains)
-{
-  print "$_\n";
-}
+print "$_\n"
+  for IServ::DB::SelectCol "SELECT WebDomain FROM scmc_servers ORDER BY WebDomain";
