ad8696a80e7ab23e7f65e2ea62106618094335fb
Felix Jacobi <felix.jacobi@iserv.eu>
2018-08-09 02:44:07 +0200

    Moved to webpack [wip]

---
+++ src/Stsbl/SchoolCertificateManagerConnectorBundle/Resources/webpack.config.js
@@ -0,0 +1,12 @@
+// src/Stsbl/SchoolCertificateManagerConnectorBundle/Resources/webpack.config.js
+let merge = require('webpack-merge');
+let path = require('path');
+let baseConfig = require(path.join(process.env.WEBPACK_BASE_PATH, 'webpack.config.base.js'));
+
+let webpackConfig = {
+    entry: {
+        'css/scmc.css': './assets/less/scmc.less',
+    },
+};
+
+module.exports = merge(baseConfig.get(__dirname), webpackConfig);
