ff490e0549a6dd380eaefdf2519a590a3fe73566
Felix Jacobi <felix.jacobi@iserv.eu>
2020-05-26 18:25:09 +0200

    Added switch for iserv-server-nginx

--- iconf/etc/apache2/sites-available/01scmc-ssl.conf/10scmc.pl
+++ iconf/etc/apache2/sites-available/01scmc-ssl.conf/10scmc.pl
@@ -6,10 +6,12 @@ use utf8;
 use IServ::DB;
 
 my @ids = IServ::DB::SelectCol "SELECT ID FROM scmc_servers";
+my $port = qx(dpkg-query --showformat='\${Status}' --show iserv-server-nginx 2> /dev/null) =~
+    /install (hold|ok) (installed|unpacked)/ ? "943" : "443";
 
 for (@ids)
 {
-  print "<VirtualHost *:443>\n";
+  print "<VirtualHost *:$port>\n";
   print "  SSLEngine on\n";
   print "  SSLProtocol all -SSLv2 -SSLv3\n";
   print "\n";
--- iconf/etc/apache2/sites-available/01scmc.conf/10scmc.pl
+++ iconf/etc/apache2/sites-available/01scmc.conf/10scmc.pl
@@ -6,10 +6,12 @@ use utf8;
 use IServ::DB;
 
 my @ids = IServ::DB::SelectCol "SELECT ID FROM scmc_servers";
+my $port = qx(dpkg-query --showformat='\${Status}' --show iserv-server-nginx 2> /dev/null) =~
+    /install (hold|ok) (installed|unpacked)/ ? "980" : "80";
 
 for (@ids)
 {
-  print "<VirtualHost *:80>\n";
+  print "<VirtualHost *:$port>\n";
   print "  Include \"scmc/$_.conf\"\n";
   print "</VirtualHost>\n";
 }
